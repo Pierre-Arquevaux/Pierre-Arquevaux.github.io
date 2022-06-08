af18dac89e94bb40a74951277ea494d398d23dff
Pierre-Arquevaux <pierre.arquevaux@gmail.com>
2022-06-08 16:14:56 +0200

    initial commit mam

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-let btnmenu = document.querySelector('#btnmenu');
-let btnclose = document.getElementById('close');
-let menu = document.querySelector('.menu');
+let navbar = document.querySelector('.navbar');
+let menubtn = document.querySelector('#menu-btn');
+let cartItem = document.querySelector('.cart-items-container');
+let cartbtn = document.querySelector('#cart-btn');
+let selection = document.querySelector('.navbar a')
 
-btnmenu.addEventListener('click',()=>{
-    menu.classList.toggle('active');
-    btnmenu.classList.toggle('active');
-    btnclose.classList.toggle('active');
+menubtn.addEventListener('click', () =>{
+    navbar.classList.toggle('active');
+    // cartItem.classList.remove('active');
+    
 });
 
-btnclose.addEventListener('click',()=>{
-    menu.classList.toggle('active');
-    btnmenu.classList.toggle('active');
-    btnclose.classList.toggle('active');
-});
+selection.addEventListener('click', () => {
+    navbar.classList.remove('active')
+})
+// cartbtn.addEventListener('click', () =>{
+//     cartItem.classList.add('active');
+//     navbar.classList.remove('active');
+// });
+
+window.onscroll= () =>{
+    navbar.classList.remove('active');
+   
+};
+
+
+
