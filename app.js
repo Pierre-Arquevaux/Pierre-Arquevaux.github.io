5c45efe8b1d1c51594117dce3f6e0c0e993fefd7
Pierre-Arquevaux <pierre.arquevaux@gmail.com>
2022-06-10 12:47:23 +0200

    ajout image + début responsive smartphone

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let menubtn = document.querySelector('#menu-btn');
 let cartItem = document.querySelector('.cart-items-container');
 let cartbtn = document.querySelector('#cart-btn');
 let selection = document.querySelector('.navbar a')
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
 menubtn.addEventListener('click', () =>{
     navbar.classList.toggle('active');
@@ -24,4 +25,21 @@ window.onscroll= () =>{
 };
 
 
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
 
